3822869f8275547375f10a0c54cbf55fb75db366
Ayoub Kadi <ayoubkadi285@gmail.com>
2023-05-10 16:50:58 +0100

    modifing the script

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -5,3 +5,12 @@ cd /etc/mysql/mariadb.conf.d/
 sed 's/127.0.0.1/0.0.0.0/g' 50-server.cnf
 
 service mysql start
+
+echo "CREATE DATABASE IF NOT EXISTS my_database;" > DB.sql
+echo "CREATE USER 'my_user'@'%' IDENTIFIED BY 'my_pass';" >> DB.sql
+echo "GRANT ALL PRIVILEGES ON my_database.* TO my_user@'%';" >> DB.sql
+echo "FLUSH PRIVILEGES;" >> DB.sql
+
+mysql < DB.sql
+
+mysqld
