8044140736bca7311294dd45aed3c5d4c93a2cda
Ayoub Kadi <ayoubkadi285@gmail.com>
2023-05-13 20:32:12 +0100

    almost done

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,16 +1,69 @@
-#!/bin/sh
-
-cd /etc/mysql/mariadb.conf.d/
-
-sed 's/127.0.0.1/0.0.0.0/g' 50-server.cnf
+#!/bin/bash
 
 service mysql start
 
-echo "CREATE DATABASE IF NOT EXISTS my_database;" > DB.sql
-echo "CREATE USER 'my_user'@'%' IDENTIFIED BY 'my_pass';" >> DB.sql
-echo "GRANT ALL PRIVILEGES ON my_database.* TO my_user@'%';" >> DB.sql
-echo "FLUSH PRIVILEGES;" >> DB.sql
+echo "create database khe_dmi default character set utf8 collate utf8_unicode_ci;" > mysql_conf
+echo "create user 'khe_dmi'@'%' identified by 'khe_dmi';" >> mysql_conf
+echo "grant all privileges on khe_dmi.* TO 'khe_dmi'@'%';" >> mysql_conf
+echo "flush privileges;" >> mysql_conf
 
-mysql < DB.sql
+#working just fine
+#echo "CREATE DATABASE IF NOT EXISTS $DB_NAME;" > mysql_conf
+##set root with the password 1234
+#echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_PW';" >> mysql_conf
+##echo "ALTER USER 'root'@'%' IDENTIFIED BY '$DB_PW';" >> mysql_conf
+##Creates a user named younes with the password 1234 and grants them access from any host
+#echo "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PW';" >>  mysql_conf
+##Grants all privileges on the word_db database to the younes use
+#echo "GRANT ALL ON $DB_NAME.* TO '$DB_USER'@'%';" >> mysql_conf
+##reloads the privileges from the grant tables in the MySQL database
+#echo "FLUSH PRIVILEGES;" >> mysql_conf
 
-mysqld
+#cat < mysql_conf
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
+
+#bash
+mysql < mysql_conf
+
+kill `cat /var/run/mysqld/mysqld.pid`
+
+mysqld_safe
+
+
+
+# #!/bin/sh
+
+# #cd /etc/mysql/mariadb.conf.d/
+
+# sed 's/127.0.0.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
+
+# service mysql start
+# # echo "create database $db_name default character set utf8 collate utf8_unicode_ci;" > mysql_conf
+# # echo "create user '$db_user'@'%' identified by '$db_pass';" >> mysql_conf
+# # echo "grant all privileges on $db_name.* TO '$db_user'@'%';" >> mysql_conf
+# # echo "flush privileges;" >> mysql_conf
+
+
+
+# echo "CREATE DATABASE IF NOT EXISTS $db_name;" > mysql_conf
+# #set root with the password 1234
+# echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$db_pass';" >> mysql_conf
+# #echo "ALTER USER 'root'@'%' IDENTIFIED BY '$DB_PW';" >> mysql_conf
+# #Creates a user named younes with the password 1234 and grants them access from any host
+# echo "CREATE USER '$db_user'@'%' IDENTIFIED BY '$db_pass';" >>  mysql_conf
+# #Grants all privileges on the word_db database to the younes use
+# echo "GRANT ALL ON $db_name.* TO '$db_user'@'%';" >> mysql_conf
+# #reloads the privileges from the grant tables in the MySQL database
+# echo "FLUSH PRIVILEGES;" >> mysql_conf
+
+# # echo "CREATE DATABASE IF NOT EXISTS $db_name;" > DB.sql
+# # echo "CREATE USER '$db_user'@'%' IDENTIFIED BY '$db_pass';" >> DB.sql
+# # echo "GRANT ALL PRIVILEGES ON $db_name.* TO $db_user@'%';" >> DB.sql
+# # echo "FLUSH PRIVILEGES;" >> DB.sql
+
+# mysql < mysql_conf
+
+# kill `cat /var/run/mysqld/mysqld.pid`
+
+# mysqld_safe
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-
+# sleep 6
 mkdir /var/www
 mkdir /var/www/html
 
-echo "<?php phpinfo(); ?>" > /var/www/html/index.php
+#echo "<?php phpinfo(); ?>" > /var/www/html/index.php
 
 sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
+apt-get update && apt-get install -y curl
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
+wp core download --path=/var/www/html --allow-root 
 
+wp config create --path=/var/www/html --dbname=$db_name --dbuser=$db_user --dbpass=$db_pass --dbhost=mariadb --allow-root --skip-check
+wp core install --url=akadi.1337.ma --title=mywebsite --admin_user=coder --admin_password=1234 --admin_email=ayoubsz200@gmail.com --allow-root
+wp user create ayoub oui@oui.monsieur --role=author --user_pass=1234 --allow-root
 mkdir /run/php
 /usr/sbin/php-fpm7.3 -F
-#php7.3-fpm --daemonize
+#php7.3-fpm --nodaemonize
 #tail -f /dev/random
